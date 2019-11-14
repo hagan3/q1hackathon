0b1cd8eb69e6a4f14acd1e10047113fa02c21f65
Ryan Hagan <hagan3@gmail.com>
2019-11-14 11:19:36 -0700

    corrected escape key

--- foregroundDetection.py
+++ foregroundDetection.py
@@ -1,37 +1,31 @@
 from __future__ import print_function
 import cv2 as cv
-import argparse
+from os import listdir
+from os.path import isfile, join
+import numpy
 
-parser = argparse.ArgumentParser(description='This program shows how to use background subtraction methods provided by \
-                                              OpenCV. You can process both videos and images.')
-parser.add_argument('--input', type=str, help='Path to a video or a sequence of image.', default='vtest.avi')
-parser.add_argument('--algo', type=str, help='Background subtraction method (KNN, MOG2).', default='MOG2')
-args = parser.parse_args()
-if args.algo == 'MOG2':
-    backSub = cv.createBackgroundSubtractorMOG2()
-else:
-    backSub = cv.createBackgroundSubtractorKNN()
-capture = cv.VideoCapture(cv.samples.findFileOrKeep(args.input))
-if not capture.isOpened:
-    print('Unable to open: ' + args.input)
-    exit(0)
-while True:
-    ret, frame = capture.read()
-    if frame is None:
-        break
 
-    fgMask = backSub.apply(frame)
+# local images folder
+mypath='./images/'
+onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
+images = numpy.empty(len(onlyfiles), dtype=object)
 
-    cv.rectangle(frame, (10, 2), (100, 20), (255, 255, 255), -1)
-    cv.putText(frame, str(capture.get(cv.CAP_PROP_POS_FRAMES)), (15, 15),
-               cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
 
-    cv.imshow('Frame', frame)
-    cv.imshow('FG Mask', fgMask)
+backSub = cv.createBackgroundSubtractorKNN()
 
-    keyboard = cv.waitKey(30)
-    if keyboard == 'q' or keyboard == 27:
-        break
+
+
+for n in range(0, len(onlyfiles)):
+    images[n] = cv.imread( join(mypath,onlyfiles[n]) )
+    fgMask = backSub.apply(images[n])
+
+    while True:
+        cv.imshow('Frame', images[n])
+        cv.imshow('FG Mask', fgMask)
+        keyboard = cv.waitKey(0)
+        if keyboard == 27:
+            break
+
 
 
 
